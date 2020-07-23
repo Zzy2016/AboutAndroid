eb58c4b8497d1616a69b1d2c87b006796a02cf5b
zhangzeyang <zhangjie@huidr.com>
2020-07-23 20:27:53 +0800

    PagerTitleStrip PagerTabStrip

--- app/src/main/java/com/example/aboutandroid/TestActivity.java
+++ app/src/main/java/com/example/aboutandroid/TestActivity.java
@@ -1,14 +1,21 @@
 package com.example.aboutandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerTabStrip;
+import androidx.viewpager.widget.PagerTitleStrip;
 
 import android.os.Bundle;
 
 public class TestActivity extends AppCompatActivity {
 
+    PagerTitleStrip pagerTitleStrip;
+
+    PagerTabStrip pagerTabStrip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pagerTitleStrip);
     }
 }
